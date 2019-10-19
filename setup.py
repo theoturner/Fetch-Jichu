03c1d8537965cd9c0902de593bfafb3f5ef4348a
Theo Turner <theo_turner@outlook.com>
2019-10-19 08:41:07 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup (
+    name = "jichu",
+    version = "0.0.1",
+    description = "Infura for Fetch.AI.",
+    license = "Apache License 2.0",
+    packages = ["jichu"],
+    install_requires = ["fetchai-ledger-api", "pytest"],
+    author = "Theo Turner",
+    author_email = "theo@turner.se",
+    keywords = ["blockchain", "infura", "fetch.ai", "fetchai", "crypto", "cryptocurrency", "hosted", "node"],
+    url = "https://github.com/theoturner/fetch-jichu",
+    classifiers = [
+        "Development Status :: 1 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
