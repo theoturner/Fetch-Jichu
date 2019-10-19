50f2492e2a9970ba8e997c723d7757b1bbcc3a5d
Theo Turner <theo_turner@outlook.com>
2019-10-19 17:00:43 +0100

    Api update

--- jichu/api.py
+++ jichu/api.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from contract import ContractDeployer
 
 app = Flask(__name__)
 # Load secret key using local config / env var in production
 app.config["SECRET_KEY"] = "session-cookie-signing-key"
-
+app.cd = ContractDeployer('127.0.0.1', 8000)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/deploy", methods = ["POST"])
+def deploy():
+    contract = request.form['contract']
+    app.cd.deploy(contract)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
