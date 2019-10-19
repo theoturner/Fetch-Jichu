382fa52c0a703d196d7f2fc0264bcd21a949f173
Theo Turner <theo_turner@outlook.com>
2019-10-19 13:56:12 +0100

    Update contract deployer

--- jichu/contract.py
+++ jichu/contract.py
@@ -11,60 +11,35 @@ class ContractDeployer:
         self.entity = Entity()
         # Create wealth so that we have the funds to be able to create contracts on the network
         self.api.sync(self.api.tokens.wealth(self.entity, 10000))
-
-        
-    def main(self):
-
-        sample_contract = """
-            persistent sharded balance : UInt64;
-            @init
-            function setup(owner : Address)
-            use balance[owner];
-            balance.set(owner, 1000000u64);
-            endfunction
-            @action
-            function transfer(from: Address, to: Address, amount: UInt64)
-            use balance[from, to];
-            
-            // Check if the sender has enough balance to proceed
-            if (balance.get(from) >= amount)
-                // update the account balances
-                balance.set(from, balance.get(from) - amount);
-                balance.set(to, balance.get(to, 0u64) + amount);
-            endif
-            endfunction
-            @query
-            function balance(address: Address) : UInt64
-                use balance[address];
-                return balance.get(address, 0u64);
-            endfunction
+    
+    def main(self, contract : str):
         """
-
-        contract = Contract(sample_contract)
-
-        with self.track_cost(self.api.tokens, self.entity, "Cost of creation: "):
-            self.api.sync(contract.create(self.api, self.entity, 4000))
+        Deploy contract. Feeds back compilation errors.
+        """
+        try:
+            contract = Contract(contract)
+            with self.track_cost(self.api.tokens, self.entity, "Cost of creation: "):
+                self.api.sync(contract.create(self.api, self.entity, 4000))
+        except Exception as e:
+            print(e)
 
 
     @contextmanager
     def track_cost(self, api: TokenApi, entity: Entity, message: str):
         """
-        Context manager for recording the change in balance over a set of actions
-        Will be inaccurate if other factors change an account balance
+        Context manager for recording the change in balance over a set of actions.
+        Will be inaccurate if other factors change an account balance.
         """
         if isinstance(entity, Entity):
             entity = Address(entity)
         elif not isinstance(entity, Address):
             raise TypeError("Expecting Entity or Address")
-
         balance_before = api.balance(entity)
         yield
-
         if not message:
             message = "Actions cost: "
-
         print(message + "{} TOK".format(api.balance(entity) - balance_before))
 
 if __name__ == '__main__':
     cd = ContractDeployer('127.0.0.1', 8000)
-    cd.main()
+    cd.main("POTATO;0%")
