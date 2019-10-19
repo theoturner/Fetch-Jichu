69c76ac4393b80605d98ec52049dcbb7b0ff3ea3
Theo Turner <theo_turner@outlook.com>
2019-10-19 15:50:32 +0100

    Add main API file

--- jichu/api.py
+++ jichu/api.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # Load secret key using local config / env var in production
 app.config["SECRET_KEY"] = "session-cookie-signing-key"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
