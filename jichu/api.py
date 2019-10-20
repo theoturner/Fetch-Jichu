be0aee7b2b6a24382878f024fad6eb09b900da00
Theo Turner <theo_turner@outlook.com>
2019-10-20 16:54:51 +0100

    Pass txhash in api

--- jichu/api.py
+++ jichu/api.py
@@ -4,17 +4,20 @@ from contract import ContractDeployer
 app = Flask(__name__)
 # Load secret key using local config / env var in production
 app.config["SECRET_KEY"] = "session-cookie-signing-key"
+txhash = ''
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", txhash = txhash)
 
 @app.route("/deploy", methods = ["POST"])
 def deploy():
+    # Server-side cookie to get around this golbal dropping Quart, no async!
+    global txhash
     address = request.form['address']
     contract = request.form['contract']
     cd = ContractDeployer(address, 8000)
-    cd.deploy(contract)
+    txhash = cd.deploy(contract)
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
